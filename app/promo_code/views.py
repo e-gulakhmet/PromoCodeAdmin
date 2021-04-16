f43304bfd6d623d54de8de2d3f1228ebf0795ee6
who-man-tech <enes515254@gmail.com>
2021-04-16 14:08:21 +0200

    Add forms to index page

--- app/promo_code/views.py
+++ app/promo_code/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import NewCodeForm
+
+
+
+def create_code(request):
+    if request.method == "POST":
+        form = NewCodeForm(request.POST)
+        if form.is_valid():
+            amount = form.cleaned_data["amount"]
+            group = form.cleaned_data["group"]
+            
+
+    form = NewCodeForm()
+    return render(
+        request,
+        "promo_code/index.html",
+        {
+            "form": form
+        })
+
