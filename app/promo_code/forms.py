0f9d4fda285bb95a5b4d61599e21d3563b0eb229
who-man-tech <enes515254@gmail.com>
2021-04-17 08:43:08 +0200

    Removed web templates for testing

--- app/promo_code/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-
-class NewCodeForm(forms.Form):
-    amount = forms.IntegerField(label="Количество кодов")
-    group = forms.CharField(label="Название группы")
--- app/promo_code/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import generate_code
-
-urlpatterns = [
-    path("", generate_code, name="generate_code")
-]
